2f5ceab9dc6309b4aa4d365f8c7f8327a3427746
home_server <lorris.belus@icloud.com>
2024-04-10 10:59:53 +0000

    add init_my_readline to module

--- readline_py/readlinemodule.c
+++ readline_py/readlinemodule.c
@@ -1,16 +1,26 @@
 #include <Python.h>
 #include "my_readline.h"
 
+
+
+static PyObject* py_init_my_readline(PyObject* self, PyObject* args)
+{
+    char* result = init_my_readline();
+    if (result == NULL)
+    {
+        Py_RETURN_NONE;
+    }
+    PyObject* ret = PyBytes_FromString(result);
+    return ret;
+}
+
+
 static PyObject* py_my_readline(PyObject* self, PyObject* args)
 {
     int fd;
     if (!PyArg_ParseTuple(args, "i", &fd))
     {
         return NULL;
-    }
-    if (rl_buff == NULL) //unlikely to work but let's try to compile and run first
-    { 
-        init_my_readline();
     }
     char* result = my_readline(fd);
     if (result == NULL)
@@ -24,6 +34,7 @@ static PyObject* py_my_readline(PyObject* self, PyObject* args)
 
 static PyMethodDef MyReadlineMethods[] = {
     {"my_readline", py_my_readline, METH_VARARGS, "Read a line from fd."},
+    {"init_my_readline", py_init_my_readline, METH_NOARGS, "Initialize buffer."},
     {NULL, NULL, 0, NULL}
 };
 
