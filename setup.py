e28db2a62d2339ff8ae4ea58bc7700938be8b025
Lorris Belus <105456131+Lbelus@users.noreply.github.com>
2024-04-10 14:30:59 +0200

    update version number

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ module = Extension('my_readline_module',
                     library_dirs = [])
 
 setup(name = 'MyReadlineModule',
-      version = '1.01',
+      version = '1.1',
       description = 'Python interface for custom readline C function',
       ext_modules = [module])
