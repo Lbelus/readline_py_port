11baada0d2a7a8424022327d668d6f6068a61fa3
home_server <lorris.belus@icloud.com>
2024-04-10 10:59:16 +0000

    simple test : read full file

--- tests/test_readlinemodule.py
+++ tests/test_readlinemodule.py
@@ -1,6 +1,10 @@
 import my_readline_module
 
+my_readline_module.init_my_readline()
 fd = open('myfile.txt', 'r')
+
 line = my_readline_module.my_readline(fd.fileno())
-print(line)
+while line != None:
+    print(line)
+    line = my_readline_module.my_readline(fd.fileno())
 fd.close()
