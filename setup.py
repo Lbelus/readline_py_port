c9ddcc67ea8127ee20725bc76b2d35356fa06664
home_server <lorris.belus@icloud.com>
2024-04-10 11:00:26 +0000

    add glob to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, Extension
+import glob
+
+sources_files = glob.glob('./readline_py/*.c')
 
 module = Extension('my_readline_module',
-                    sources = ['./readline_py/readlinemodule.c'],
-                    include_dirs = ['./readline_py/'],  
+                    sources = sources_files,
+                    include_dirs = ['./readline_py/my_readline.h'],  
                     libraries = [], 
                     library_dirs = [])
 
 setup(name = 'MyReadlineModule',
-      version = '1.0',
+      version = '1.01',
       description = 'Python interface for custom readline C function',
       ext_modules = [module])
