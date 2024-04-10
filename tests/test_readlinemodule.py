74808b2825a2767d19e530afac2064b823caf21d
home_server <lorris.belus@icloud.com>
2024-04-10 11:58:40 +0000

    modify simple read test to match python expected syntax

--- tests/test_readlinemodule.py
+++ tests/test_readlinemodule.py
@@ -3,8 +3,9 @@ import my_readline_module
 my_readline_module.init_my_readline()
 fd = open('myfile.txt', 'r')
 
-line = my_readline_module.my_readline(fd.fileno())
-while line != None:
+line = None
+
+while (line := my_readline_module.my_readline(fd.fileno())) is not None:
     print(line)
-    line = my_readline_module.my_readline(fd.fileno())
+
 fd.close()
